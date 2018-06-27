6a9a83fa0aa168c10c69c97fbc576ffd62b2d149
Eugene <Eugene@_-_.local>
2018-06-27 09:20:14 +0300

    byte stream working. base64 not complete

---
+++ src/main/java/net/honeyflower/pictshare/client/PictShareClient.java
@@ -0,0 +1,76 @@
+package net.honeyflower.pictshare.client;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class PictShareClient {
+	
+	private final RestTemplate restTemplate;
+	
+	@Value("${pictshare.image.hosting.upload_url}")
+	private String baseUrl;
+	
+	private static final List<MediaType> accepts = Arrays.asList(MediaType.APPLICATION_JSON);
+	private static final String userAgent = "honeyflower-systems";
+	 
+    public PictShareClient(RestTemplateBuilder restTemplateBuilder) {
+    	ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+            request.getHeaders().set(HttpHeaders.USER_AGENT, userAgent);
+            request.getHeaders().setAccept(accepts);
+            return execution.execute(request, body);
+        };
+        
+        restTemplate = restTemplateBuilder.rootUri(baseUrl).additionalInterceptors(interceptor).build();
+    }
+    
+    public PictShareClient() {
+    	this(new RestTemplateBuilder());
+    }
+    
+    public String uploadBase64(String base64Image) {
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+    	MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+    	map.add("base64", base64Image);
+
+    	HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+    	ResponseEntity<String> response = restTemplate.postForEntity(baseUrl, request , String.class );
+    	return response.getBody();
+    }
+    
+    public String upload(byte[] image) {
+    	ByteArrayResource fileAsResource = new ByteArrayResource(image) {
+            @Override
+            public String getFilename() {
+                return "lalala";
+            }
+        };
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("postimage", fileAsResource);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+        ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.POST, requestEntity, String.class);
+        return response.getBody();
+    }
+
+}
---
+++ src/test/java/net/honeyflower/pictshare/client/DummyConfiguration.java
@@ -0,0 +1,12 @@
+package net.honeyflower.pictshare.client;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan
+@EnableAutoConfiguration
+public class DummyConfiguration {
+
+}
